d308d9839d9574c654000296db447c395b3478cc
tubashov <tubashov@yandex.ru>
2025-06-05 22:57:32 +0300

    Checkstyle ManagerSaveException

--- src/taskmanager/controller/ManagerSaveException.java
+++ src/taskmanager/controller/ManagerSaveException.java
@@ -1,11 +1,11 @@
 package taskmanager.controller;
 
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException (String message, Throwable cause) {
-        super(message, cause);
-    }
 
-    public  ManagerSaveException (String message) {
+    public  ManagerSaveException(String message) {
         super(message);
+    }
+    public ManagerSaveException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
