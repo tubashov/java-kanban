edb27f886d6666b94e40270c2c5caa39a7fadca3
tubashov <tubashov@yandex.ru>
2025-06-09 17:44:48 +0300

    Удаление неиспользуемого конструктора из класса ManagerSaveException.

--- src/taskmanager/exceptions/ManagerSaveException.java
+++ src/taskmanager/exceptions/ManagerSaveException.java
@@ -2,11 +2,8 @@ package taskmanager.exceptions;
 
 public class ManagerSaveException extends RuntimeException {
 
-    public  ManagerSaveException(String message) {
-        super(message);
-    }
-
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
+
 }
