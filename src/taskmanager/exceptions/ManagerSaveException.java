a38a08659fb54f6191ce4dedf9dca3cfb55b2935
tubashov <tubashov@yandex.ru>
2025-06-10 16:14:00 +0300

    Исправление ошибок.

--- src/taskmanager/exceptions/ManagerSaveException.java
+++ src/taskmanager/exceptions/ManagerSaveException.java
@@ -2,6 +2,10 @@ package taskmanager.exceptions;
 
 public class ManagerSaveException extends RuntimeException {
 
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
