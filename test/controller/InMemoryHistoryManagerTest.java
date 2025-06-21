09de815b2fff390a6376d9bc8c0321a4adc36877
tubashov <tubashov@yandex.ru>
2025-06-21 19:32:30 +0300

    Исправление ошибок.

--- test/controller/InMemoryHistoryManagerTest.java
+++ test/controller/InMemoryHistoryManagerTest.java
@@ -49,7 +49,7 @@ public class InMemoryHistoryManagerTest {
         List<Task> history = historyManager.getHistory();
 
         assertEquals(1, history.size());
-        assertEquals(task1, history.get(0));
+        assertEquals(task1, history.getFirst());
     }
 
     @Test
